d238284d674ca1d97fb1373d682eda3a803bea9b
Vibhujay99 <vibhujay99@gmail.com>
2023-10-10 02:35:11 +0530

    Ref() : App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./Router/MainRouter";
+import { isAuthenticated } from "./Utils/Requests/Auth";
+import setAuthToken from "./Utils/setAuthToken";
+import "./App.scss";
 
-import './App.css';
+setAuthToken(isAuthenticated().token);
 
-function App() {
-  return (
-    <div className="App">
-      <h1> AAAAAA </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <MainRouter />
+      </BrowserRouter>
+    );
+  }
 }
-
 
 export default App;
